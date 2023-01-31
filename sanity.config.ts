b30e132f06562e45210e7f9d183085acbb9d6126
Naheen Habib <naheenhabib@outlook.com>
2023-01-31 19:11:26 +1100

    new commit

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,19 +3,16 @@ import { deskTool } from 'sanity/desk'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './schemas'
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
 
 export default defineConfig({
   basePath: "/studio",
-  name: 'default',
-  title: 'nextJS Naheen Blog',
-
-  projectId: 'nv02lb2s',
-  dataset: 'production',
-
+  name: "NAHEEN_BLOG",
+  title: "NAHEEN_BLOG",
+  projectId,
+  dataset,
   plugins: [deskTool(), visionTool()],
-
   schema: {
     types: schemaTypes,
   },
